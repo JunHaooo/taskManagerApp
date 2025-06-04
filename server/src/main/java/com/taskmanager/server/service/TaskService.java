c615b4eee2f7ab962daeb4d4592c8205eaa6695e
JunHaooo <strongboyljh@gmail.com>
2025-06-04 15:32:02 +0800

    added h2 database dependency in pom.xml

--- server/src/main/java/com/taskmanager/server/service/TaskService.java
+++ server/src/main/java/com/taskmanager/server/service/TaskService.java
@@ -1,9 +1,12 @@
-import com.taskmanager.server.repository.TaskRepository;
-import com.taskmanager.server.model.Task; // Also make sure you import the Task model
+package com.taskmanager.server.service;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.taskmanager.server.model.Task;
+import com.taskmanager.server.repository.TaskRepository;
 
 @Service
 public class TaskService {
